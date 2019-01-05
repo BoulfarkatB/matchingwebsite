b94606d6179f5b45f6d8dc6f109cb08b60ba09db
Bassem Boulfarkat <bisbis@Bassems-MacBook-Pro.local>
2019-01-05 20:48:20 +0000

    heroku push

--- matchingwebsite/matching_website/settings.py
+++ matchingwebsite/matching_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7__ldo@rla(5%&k8xh&rai_$(yek-dq$-_ctl2=v$nl-=!tza2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost','127.0.0.1','https://sheltered-fjord-72392.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
